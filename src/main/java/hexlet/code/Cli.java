da89a61bd1547be504ae81def1308a83d0d8f6b5
sadamara <andrewgalkin0303@gmail.com>
2022-05-03 15:08:58 +0300

    stage 3-done

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+     private static String name;
+
+     public Cli(String name) {
+         this.name = name;
+     }
+     public static String getName() {
+         Scanner scanner = new Scanner(System.in);
+         String name = scanner.nextLine();
+         return name;
+     }
+
+}
