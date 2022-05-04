f10ff3c3e24c953398cd3e70c8141e7a8efbf82b
sadamara <andrewgalkin0303@gmail.com>
2022-05-04 12:44:54 +0300

    add checkstyle

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -2,15 +2,15 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-     private static String name;
+    private static String name;
 
-     public Cli(String name) {
-         this.name = name;
-     }
-     public static String getName() {
-         Scanner scanner = new Scanner(System.in);
-         String name = scanner.nextLine();
-         return name;
-     }
+    public Cli(String name) {
+        this.name = name;
+    }
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        return name;
+    }
 
 }
