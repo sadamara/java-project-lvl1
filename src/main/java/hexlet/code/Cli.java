e93fa7e14b6288d67be6df8b8285fe5fceb327b4
sadamara <andrewgalkin0303@gmail.com>
2022-05-04 14:56:00 +0300

    test Github Actions

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,9 @@ import java.util.Scanner;
 
 public class Cli {
     private static String name;
-
-    public Cli(String name) {
-        this.name = name;
-    }
     public static String getName() {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        return name;
+        return scanner.nextLine();
     }
 
 }
