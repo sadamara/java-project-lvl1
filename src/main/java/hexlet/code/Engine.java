656686cb7e05dd11120aff57db31e0916aae8542
sadamara <andrewgalkin0303@gmail.com>
2022-05-18 12:41:59 +0300

    update Engine and Calc game

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -18,7 +18,7 @@ public class Engine {
     }
 
     public static int getRandomNumber() {
-        final var finish = 30;
+        final var finish = 20;
         return (1 + (int) (Math.random() * finish));
     }
 
@@ -37,14 +37,41 @@ public class Engine {
         return scanner.nextInt();
     }
 
-    public static int getRandomOperator() {
-        final var finish = 3;
-        return (1 + (int) (Math.random() * finish));
+    public static String getRandomOperator() {
+        String[] operators = {"+", "-", "*"};
+        final int max = 3;
+        int i = (0 + (int) (Math.random() * max));
+        return operators[i];
     }
 
     public static int getRandomLengthProgression() {
         final var start = 5;
         final var finish = 10;
         return (start + (int) (Math.random() * finish));
+    }
+
+    public static final int BESTSCORE = 3;
+
+    public static void runEngine(String question, String[][] questionAndAnswer) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!\n" + "May I have your name?");
+        String playerName = scanner.nextLine();
+        System.out.println("Hello, " + playerName + "!");
+        System.out.println(question);
+
+        for (int i = 0; i < BESTSCORE; i++) {
+            System.out.println("Question: " + questionAndAnswer[i][0]);
+            String playerAnswer = scanner.nextLine();
+            System.out.println("Your answer: " + playerAnswer);
+            if (playerAnswer.equals(questionAndAnswer[i][1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + playerAnswer + "'" + " is wrong answer. Correct answer was "
+                        + "'" + questionAndAnswer[i][1] + "'" + ". Let's try again, " + playerName + "!");
+                return;
+            }
+        }
+
+        System.out.println("Congratulations, " + playerName + "!");
     }
 }
--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -3,60 +3,40 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
+    private static final String QUESTION = "What is the result of the expression?";
+    private static final int COUNTER = 1;
+
     public static void runGame() {
-        Engine.getGreet();
-        String name = Engine.getName();
-        System.out.println("What is the result of the expression?");
-        var i = 0;
-        final int bestScore = 3;
-
-        while (i < bestScore) {
-            int randomNumberOne = Engine.getRandomNumber();
-            int randomNumberTwo = Engine.getRandomNumber();
-            var plus = randomNumberOne + randomNumberTwo;
-            var minus = randomNumberOne - randomNumberTwo;
-            var multiply = randomNumberOne * randomNumberTwo;
-            final var plusRandom = 1;
-            final var minusRandom = 2;
-            final var multiplyRandom = 3;
-
-            if (Engine.getRandomOperator() == plusRandom) {
-                System.out.println("Question: " + randomNumberOne + " + " + randomNumberTwo);
-                int answer = Engine.getUsersAnswerInt();
-                if (answer == plus) {
-                    System.out.println("Correct!");
-                    i++;
-                } else {
-                    System.out.println(answer + " is wrong answer ;(. Correct answer was " + plus);
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (Engine.getRandomOperator() == minusRandom) {
-                System.out.println("Question: " + randomNumberOne + " - " + randomNumberTwo);
-                int answer2 = Engine.getUsersAnswerInt();
-                if (answer2 == minus) {
-                    System.out.println("Correct!");
-                    i++;
-                } else {
-                    System.out.println(answer2 + " is wrong answer ;(. Correct answer was " + minus);
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            } else if (Engine.getRandomOperator() == multiplyRandom) {
-                System.out.println("Question: " + randomNumberOne + " * " + randomNumberTwo);
-                int answer3 = Engine.getUsersAnswerInt();
-                if (answer3 == multiply) {
-                    System.out.println("Correct!");
-                    i++;
-                } else {
-                    System.out.println(answer3 + " is wrong answer ;(. Correct answer was " + multiply);
-                    System.out.println("Let's try again, " + name + "!");
-                    break;
-                }
-            }
+        String[][] questionAndAnswer = new String[Engine.BESTSCORE][2];
+        for (int i = 0; i < Engine.BESTSCORE; i++) {
+            int firstNumber = Engine.getRandomNumber();
+            int secondNumber = Engine.getRandomNumber();
+            String randomOperator = Engine.getRandomOperator();
+            String question = firstNumber + " " + randomOperator + " " + secondNumber;
+            questionAndAnswer[i][0] = question;
+            questionAndAnswer[i][COUNTER] = String.valueOf(
+                    getTrueAnswer(randomOperator, firstNumber, secondNumber));
         }
-        if (i == bestScore) {
-            System.out.println("Congratulations, " + name + "!");
+
+        Engine.runEngine(QUESTION, questionAndAnswer);
+    }
+
+    private static int getTrueAnswer(String randomOperator, int firstNumber, int secondNumber) {
+        int answer = 0;
+        switch (randomOperator) {
+            case "+":
+                answer = firstNumber + secondNumber;
+                break;
+            case "-" :
+                answer = firstNumber - secondNumber;
+                break;
+            case "*":
+                answer = firstNumber * secondNumber;
+                break;
+            default:
+                System.out.println("Unexpected value");
+                break;
         }
+        return answer;
     }
 }
